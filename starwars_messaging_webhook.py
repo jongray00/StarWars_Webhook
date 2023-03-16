be5fc87c623f75ba48652c0e66ec92967964d04d
jongray00 <78746011+jongray00@users.noreply.github.com>
2023-03-16 06:26:09 -0700

    Update starwars_messaging_webhook.py

--- starwars_messaging_webhook.py
+++ starwars_messaging_webhook.py
@@ -4,13 +4,6 @@ from flask import Flask, request
 from signalwire.messaging_response import MessagingResponse
 from signalwire.rest import Client as signalwire_client
 
-# Input User Authentication into Client
-ProjectID= "649dc08e-3558-4efe-a598-46a966164b83"
-AuthToken= "PTdaf6a2203f03e081317bc3c4dae79542fd737ae9cf49db60"
-SpaceURL = 'jon-gray.signalwire.com'
-sw_phone_number= '+12022214915'
-
-client = signalwire_client(ProjectID, AuthToken , signalwire_space_url = SpaceURL)
 
 # Initialize the Flask object
 app = Flask(__name__)
@@ -42,10 +35,6 @@ def start_ngrok():
     # Set up a tunnel on port 5000 for our Flask object to interact locally
     url = ngrok.connect(5000).public_url
     print(' * Tunnel URL:', url)
-    # Now that we have our URL, Use the SignalWire's Update an Incoming Phone Number API
-    client.incoming_phone_numbers.list(
-        phone_number= sw_phone_number)[0].update(
-            sms_url=url + '/sms_app') # Notice we update the parameter sms_url
 
 # In the previous step, we declared where the tunnel will be opened, however we must start ngrok before a tunnel will be available to open
 # This checks your os to see if ngrok is already running and if it isnt, will begin running
